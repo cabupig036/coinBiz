6e58cd9560c0acca36b164ce1cae61c305fc49c5
NhatTruong-dotnet <55442462+NhatTruong-dotnet@users.noreply.github.com>
2021-02-05 16:43:10 +0700

    add EditDelete Button

--- ConBiz/JS/DatatableScript.js
+++ ConBiz/JS/DatatableScript.js
@@ -23,13 +23,13 @@ function createTableItem(itemName) {
 }
 
 function getDataToTable(data) {
-
     $.each(data, (index, element) => {
         let table = $('#example tbody');
         let items = [];
         $.each(element, (index, item) => {
             items.push("<td>" + item + "</td>");
         });
+        items.push('<td><i class="fas fa-edit" onclick="UpdateItem('+element.id+')"></i>/<i class="fas fa-user-minus" onclick="DeleteItem('+element.id+')"></i></td>')
         $("<tr/>", {
             html: items.join("")
         }).appendTo(table);
