97eda2e7ee2f26919db2c43f81a6bd93da2c2d76
cabupig036 <71782129+cabupig036@users.noreply.github.com>
2021-02-10 00:47:19 +0700

    Add files via upload

---
+++ ConBiz/jquery/index.js
@@ -0,0 +1,13 @@
+    function SomeDeleteRowFunction(o) {
+     //no clue what to put here?
+     var p=o.parentNode.parentNode;
+         p.parentNode.removeChild(p);
+    }
+function myCreateFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
